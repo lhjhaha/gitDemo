97a8da814e4fa5f0056ed3fa8ffdbff3cbec2aec
lhj <2090917504@qq.com>
2023-03-18 11:37:01 +0800

    hot_fix commit1

--- src/main/java/com/atguigu/GuiguTest.java
+++ src/main/java/com/atguigu/GuiguTest.java
@@ -3,5 +3,6 @@ package com.atguigu;
 public class GuiguTest {
     public static void main(String[] args) {
         System.out.println("hahaha");
+        System.out.println("hello git");
     }
 }
